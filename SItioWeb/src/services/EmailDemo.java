729a44b5a4cd0dc8cd445be6ec83a567f0fb861a
JORGE\Jorge1996 <jorgegimenez_1996@live.com>
2017-11-17 10:39:19 -0300

    Puse las clases que permiten mandar email, falta implementar la logica

---
+++ SItioWeb/src/services/EmailDemo.java
@@ -0,0 +1,14 @@
+package services;
+public class EmailDemo {
+
+	public static void main(String[] args) {
+		String to [] = {"jorgegimenez_1996@live.com"};
+		if(EmailSenderService.sendEmail
+				("jorgehgimenez.1996@gmail.com", "*******", 
+						"Probando mail desde java", 
+						to))System.out.println("email sent successfully");
+		else System.out.println("Some error occurred");
+
+	}
+
+}
--- SItioWeb/src/views/LoginView.java
+++ SItioWeb/src/views/LoginView.java
@@ -15,6 +15,7 @@ import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Link;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
@@ -105,6 +106,8 @@ public class LoginView extends VerticalLayout implements View {
 		HorizontalLayout actions = new HorizontalLayout(bttInicSesion, bttRegistrarse);
 		actions.setSpacing(true);
 		formLogin.addComponent(actions);
+		
+		
 		
 		addStyleName("fondoPantalla");
 		panel.addStyleName("fondoPanel");
